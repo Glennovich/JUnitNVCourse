47666a6d7b71ef5eb73acefb4e867862e658934a
Glennovich <g.degrendele.work@gmail.com>
2019-07-28 22:56:05 +0200

    opdracht 9: Mock-objecten

--- src/test/java/be/nvcursus/JUnit/ThermostatTestWithMocks.java
+++ src/test/java/be/nvcursus/JUnit/ThermostatTestWithMocks.java
@@ -7,33 +7,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 /**
- * ThermostatTest
- * Here, the class is tested using Stubs
- * A stub is an object that replaces another object in a test for the purpose of being
- * able to determine its behaviour in the test. Here we created the SensorStub in order
- * to be able to set the temperature it will return, allowing us to simulate multiple test
- * conditions.
+ * ThermostatTestWithMocks
+ * Here, the class is tested using Mocks
+ * A mock is an object that replaces another object for the purpose of testing, just like
+ * a stub is, but with the added difference that we can also check whether the communication
+ * between the object we're testing and our mock is going as expected.
  * @author gdegr_000
  *
  */
 @DisplayName("Thermostat should")
-class ThermostatTest {
+class ThermostatTestWithMocks {
 	private final static int INTERVAL = 100; //course says 10, but should be 100 because default interval in Thermostat is 100 and we want to wait longer than 1 thread cycle
 	private Thermostat thermostat;
-	private HeatingStub heatingStub;
-	private SensorStub sensorStub;
+	private HeatingMock heatingMock;
+	private SensorMock sensorMock;
 	
-	private class HeatingStub implements Heating {
+	private class HeatingMock implements Heating {
+		private boolean status;
+		
 		public void setHeating(boolean status) {
-			//do nothing, we only need a class that seems to implement Heating
+			this.status = status;
+		}
+		
+		//extra mock method to verify
+		public boolean isHeating() {
+			return status;
 		}
 	}
 	
-	private class SensorStub implements Sensor {
+	private class SensorMock implements Sensor {
 		private float temp;
+		private boolean called;
 		
 		public Temperature getTemperature() {
 			try {
+				called = true;
 				return new Temperature(temp);
 			} catch (InvalidTemperatureException e) {
 				e.printStackTrace();
@@ -41,16 +49,26 @@ class ThermostatTest {
 			return null;
 		}
 		
+		//stub method to control stub
 		public void setTemp(float temp) {
 			this.temp = temp;
+		}
+		
+		//extra mock methods to verify
+		public boolean isCalled() {
+			return called;
+		}
+		
+		public void setCalled(boolean status) {
+			called = status;
 		}
 	}
 	
 	@BeforeEach
 	public void init() {
-		sensorStub = new SensorStub();
-		heatingStub = new HeatingStub();
-		thermostat = new Thermostat(heatingStub, sensorStub);
+		sensorMock = new SensorMock();
+		heatingMock = new HeatingMock();
+		thermostat = new Thermostat(heatingMock, sensorMock);
 		thermostat.setInterval(INTERVAL);
 		thermostat.start();
 	}
@@ -64,35 +82,56 @@ class ThermostatTest {
 	@DisplayName("be heating when the sensor temperature is lower than target temperature")
 	public void testThermostat() throws InterruptedException, InvalidTemperatureException {
 		thermostat.setTargetTemperature(new Temperature(20));
-		sensorStub.setTemp(15);
+		sensorMock.setTemp(15);
+		sensorMock.setCalled(false);
+		heatingMock.setHeating(false);
 		Thread.sleep(INTERVAL*3);
 		assertTrue(thermostat.isHeating());
+		assertTrue(sensorMock.isCalled());
+		assertTrue(heatingMock.isHeating());
 	}
 
 	@Test
 	@DisplayName("stop heating when temperature hits target and start heating when temperature goes below target")
 	public void testChangeCurrent() throws InterruptedException, InvalidTemperatureException {
 		thermostat.setTargetTemperature(new Temperature(20));
-		sensorStub.setTemp(20);
+		sensorMock.setTemp(20);
+		sensorMock.setCalled(false);
+		heatingMock.setHeating(false);
 		Thread.sleep(INTERVAL*3);
 		assertFalse(thermostat.isHeating());
-		sensorStub.setTemp(19.0F);
+		assertTrue(sensorMock.isCalled());
+		assertFalse(heatingMock.isHeating());
+		sensorMock.setTemp(19.9F);//borderline value testing
+		sensorMock.setCalled(false);//re-initialize our sensorMock so we'll be able to verify whether it was called again
 		Thread.sleep(INTERVAL*3);
 		assertTrue(thermostat.isHeating());
+		assertTrue(sensorMock.isCalled());
+		assertTrue(heatingMock.isHeating());
 	}
 	
 	@Test
 	@DisplayName("stop heating when target temperature is lowered to current temperature or lower and start heating when target is raised")
 	public void testChangeTarget() throws InterruptedException, InvalidTemperatureException {
 		thermostat.setTargetTemperature(new Temperature(25));
-		sensorStub.setTemp(20.7F);
+		sensorMock.setTemp(20.7F);
+		sensorMock.setCalled(false);
+		heatingMock.setHeating(false);//it would be heating in this case, but here we set the initial situation so we can be sure that it was changed by the thermostat later
 		Thread.sleep(INTERVAL*3);
 		assertTrue(thermostat.isHeating());
+		assertTrue(sensorMock.isCalled());
+		assertTrue(heatingMock.isHeating());
 		thermostat.setTargetTemperature(new Temperature(20.7F));
+		sensorMock.setCalled(false);//re-initialize our sensorMock so we'll be able to verify whether it was called again
 		Thread.sleep(INTERVAL*3);
 		assertFalse(thermostat.isHeating());
+		assertTrue(sensorMock.isCalled());
+		assertFalse(heatingMock.isHeating());
 		thermostat.setTargetTemperature(new Temperature(20.8F));
+		sensorMock.setCalled(false);//re-initialize our sensorMock so we'll be able to verify whether it was called again
 		Thread.sleep(INTERVAL*3);
 		assertTrue(thermostat.isHeating());
+		assertTrue(sensorMock.isCalled());
+		assertTrue(heatingMock.isHeating());
 	}
 }
